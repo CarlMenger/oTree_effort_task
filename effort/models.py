31a633d1728417514a813e3a3b8646ca5e670b0a
CarlMenger <56433847+CarlMenger@users.noreply.github.com>
2020-09-07 23:34:02 +0200

    add questionaire

--- effort/models.py
+++ effort/models.py
@@ -8,8 +8,10 @@ from otree.api import (
     Currency as c,
     currency_range,
 )
-import copy
+import pandas
 import logging
+import time
+
 logging.basicConfig(level=logging.DEBUG,
                     filename="example2.log",
                     format='%(asctime)s %(message)s')
@@ -25,7 +27,7 @@ Separate players based on real effort task into group competing against each oth
 class Constants(BaseConstants):
     name_in_url = "StopLookingAtUrl"
     players_per_group = None
-    num_rounds = 2
+    num_rounds = 3
     grouping_order_keywords = {
         "score_ahead_high": "You are significantly ahead in score compared to the other player in the group",
         "score_ahead_low": "You are slightly ahead in score compared to the other player in the group",
@@ -33,61 +35,35 @@ class Constants(BaseConstants):
         "score_behind_low": "You are slightly behind in score compared to the other player in the group",
         "score_behind_high": "You are significantly behind in score compared to the other player in the group",
     }
-    # TaskStage variables
 
-    def countdown_timer(self):
-        return 5
-
-    score_position_bound = 0.2# 0.2 == 20 % difference
 
 class Subsession(BaseSubsession):
+
     def point_score_everyone(self):
         return [p.point_score for p in self.get_players()]
 
     def show_player_matrix(self):
         return self.get_group_matrix()
 
-    def create_group_matrix(self):
-        # find if starting from index 0 or 1 gives less average differences between pairs
-        def pairing_algorithm(scores_all):
-            pair_combinations = []
-            for index in range(0, len(scores_all), 2):
-                try:
-                    pair_combinations.append([scores_all[index], scores_all[index + 1]])
-                except IndexError:
-                    # except is point pairing if uneven players, adding second highest score to be paired with 3rd and 1 st
-                    pair_combinations.append([scores_all[index], scores_all[index - 1]])
+    def calculate_payoff_table(self):
+        pass
 
-            return pair_combinations
+    def create_record_files(self):
+        def get_records():
+            records = ([[player.point_score for player in player.in_all_rounds()] for player in
+                    self.get_players()])
+            print(records)
+            records_df = pandas.DataFrame(records)
+            records_df.to_csv("score_records.csv")
 
-        # add ids to point matrix
-        def create_point_ids(points_all):
-            return dict(enumerate(points_all, start=1))
+        def get_payfile():
+            payfile = [player.get_payoff() for player in self.get_players()]
+            print(payfile)
+            payfile_df = pandas.DataFrame(payfile)
+            payfile_df.to_csv("payfile.txt")
 
-        # points_ids --> dict of id:points, point_matrix --> list of list of points (same as group_matrix)
-        def convert_points_to_ids(points_ids, point_matrix):
-            for group_index in range(len(point_matrix)):
-                for point_index in range(len(point_matrix[group_index])):
-                    value = point_matrix[group_index][point_index]
-                    try:
-                        key = (list(points_ids.keys())[list(points_ids.values()).index(value)])
-                        point_matrix[group_index][point_index] = key
-                        points_ids.pop(key, -999)
-                    except ValueError:
-                        # if uneven players, point matrix has one more element than ids, uses last
-                        point_matrix[group_index][point_index] = point_matrix[group_index - 1][point_index]
-            return point_matrix
-
-        points_all = self.point_score_everyone()
-        point_matrix = pairing_algorithm(sorted(points_all))
-        points_ids = create_point_ids(points_all)
-        new_group_matrix = convert_points_to_ids(points_ids, point_matrix)
-        return new_group_matrix
-
-    def group_based_on_score(self):
-        if self.round_number == 1:  # regrouping limited to first game
-            output = self.create_group_matrix()
-            self.set_group_matrix(output)
+        get_records()
+        get_payfile()
 
 
 class Group(BaseGroup):
@@ -100,26 +76,15 @@ class Player(BasePlayer):
     task_stage_timeout_seconds = models.IntegerField(initial=35)
 
     def other_player_score(self):
-        return self.get_others_in_group()[0].point_score
+        pass
+        #TODO
 
     def player_point_score(self):
         return self.point_score
 
-    def get_score_position(self):
-        other_player_score = self.other_player_score()
-        try:
-            score_difference = (other_player_score - self.point_score) /\
-                           (self.point_score + other_player_score / 2)
-        except ZeroDivisionError:
-            score_difference = 0
+    def participant_label(self):
+        return self.participant.label
 
-        if score_difference >= Constants.score_position_bound:
-            return Constants.grouping_order_keywords["score_ahead_high"]
-        elif 0.2 > score_difference > 0:
-            return Constants.grouping_order_keywords["score_ahead_low"]
-        elif score_difference == 0:
-            return Constants.grouping_order_keywords["score_equal"]
-        elif 0 > score_difference > - Constants.score_position_bound:
-            return Constants.grouping_order_keywords["score_behind_low"]
-        elif - 0.2 >= score_difference:
-            return Constants.grouping_order_keywords["score_behind_high"]
+    def get_payoff(self):
+        return sum([player_in_all_rounds.point_score for player_in_all_rounds in self.in_rounds(2, 3)]) * \
+               self.session.config["conversion_rate"] + self.session.config["participation_fee"]
--- settings.py
+++ settings.py
@@ -1,21 +1,34 @@
 from os import environ
 
+
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
 # in SESSION_CONFIGS, except those that explicitly override it.
 # the session config can be accessed from methods in your apps as self.session.config,
 # e.g. self.session.config["participation_fee"]
 
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=1.00, participation_fee=50.00, doc="", use_browser_bots=False
+    real_world_currency_per_point=5.00, participation_fee=50.00, doc="", use_browser_bots=False, conversion_rate=0.50,
 )
 
 SESSION_CONFIGS = [
      dict(
-        name="effort_task_demo",
+        name="effort_task_demo_1",
         display_name="Experiment_demo",
         num_demo_participants=2,
         app_sequence=['effort'],
+        treatment="0",
+        participation_fee=50,
+        conversion_rate=0.50,
      ),
+    dict(
+        name="effort_task_demo_2",
+        display_name="Experiment_demo_2",
+        num_demo_participants=2,
+        app_sequence=['effort'],
+        treatment="1",
+        conversion_rate=0.50
+
+    ),
 ]
 
 
@@ -24,14 +37,14 @@ SESSION_CONFIGS = [
 LANGUAGE_CODE = "en"
 
 # e.g. EUR, GBP, CNY, JPY
-REAL_WORLD_CURRENCY_CODE = "USD"
+REAL_WORLD_CURRENCY_CODE = "CZK"
 USE_POINTS = True
 
 ROOMS = [
     dict(
         name='effort_room_demo',
         display_name='Effort_room_test',
-        #participant_label_file='_rooms/econ101.txt',
+        participant_label_file='_rooms/participant_label_file.txt',
         use_secure_urls=False
     ),
 ]
